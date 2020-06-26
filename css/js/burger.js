007cf45b37a34d64e27dffcdf8489e215a41e960
dalebsellers <dalesellers@sbcglobal.net>
2020-06-26 15:35:52 -0400

    added folders and files

---
+++ css/js/burger.js
@@ -0,0 +1,11 @@
+// a mobile navigation solution
+// for playulti.org
+
+// Acknowledgements below the js
+
+$('.Navigation--collapsed').click(function() {
+    $('.Overlay').toggleClass('Overlay--on');
+    $('.BurgerMenu').toggleClass('BurgerMenu--on');
+  });
+  
+  // Inspiration and learning from: Paul Sullivan https://codepen.io/pwsm50/pen/KHDEI and Paul van Oijen https://codepen.io/PaulVanO/pen/XJYGNQ
